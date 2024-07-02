7ab522cff941289afadc6de14f75178098f4a51e
Brian <brianhabana@gmail.com>
2024-07-02 07:51:27 -0700

    Aura User Widget and Widget Controller

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Copyright Brian Habana
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
