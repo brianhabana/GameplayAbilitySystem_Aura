f22acaf8550a60d31971440e380428d75496a919
Brian <brianhabana@gmail.com>
2024-10-07 14:29:16 -0700

    Gameplay Tags Singleton

---
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -0,0 +1,22 @@
+// Copyright Brian Habana
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+
+/**
+ *  Aura GameplayTags
+ *
+ *  Singleton containing native Gameplay Tags
+ */
+struct FAuraGameplayTags
+{
+public:
+ static const FAuraGameplayTags& Get() { return GameplayTags;}
+ static void InitializeNativeGameplayTags();
+protected:
+
+private:
+ static FAuraGameplayTags GameplayTags;
+};
